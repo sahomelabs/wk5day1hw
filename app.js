37c069cce4c7aba4d33f78f25ac9680a1af255fb
sahomelabs <sahomelabs@gmail.com>
2023-11-15 19:32:43 -0500

    update app-js data structure

---
+++ app.js
@@ -0,0 +1,7 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
